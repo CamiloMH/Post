7bbf03a4ef6c5461adccd42872daad741d6241d9
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-10-26 16:26:33 -0300

    Test CRUD userService

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
+import { compileNgModuleDeclarationExpression } from '@angular/compiler/src/render3/r3_module_compiler';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
+
+  user = {
+    name: 'asd',
+    lastName: 'Hola',
+    mail: 'frontend@gmail.com',
+    password: '123456'
+  }
 
   ngOnInit(): void {
+
+    this.userService.loginUser(this.user).then((resp) => {
+      console.log(resp);
+
+    })
+    /* //Create user
+    this.userService.createUser(this.user).then((data) => {
+      console.log({ data });
+    }) */
+
+    setTimeout(() => {
+      /* //Delete user
+      this.userService.deleteUser('61770a442ce9b52c00a848f5').then((data) => {
+        console.log(data); */
+      /* //get Users
+      this.userService.getUser().then(({ data }) => {
+        console.log(data); */
+      // })
+      // })
+      /* //Logout
+      this.userService.logout(); */
+      this.userService.getUserById('6173208167e399900aa7a2a0').then(({ data }) => {
+        console.log(data);
+
+      })
+    }, 4000)
+
+
   }
 
 }
